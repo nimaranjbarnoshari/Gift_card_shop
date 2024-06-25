a8aad9ef430a6c49cc89d09b25c51c9d1419d01e
Nima Ranjbar <nimaranjbarnoshari@gmail.com>
2024-06-25 09:27:21 +0330

    feat: start develope dashboard page(grid and background and title)

--- src/Pages/Panel/Dashboard/Dashboard.jsx
+++ src/Pages/Panel/Dashboard/Dashboard.jsx
@@ -3,7 +3,13 @@ import "./Dashboard.css";
 export default function Dashboard() {
   return (
     <div className="panel-styles dashboard">
-      <h2>Dashboard</h2>
+      <div className="dashboard-right">
+      <h2 className="dashboard-right__title">داشبورد کاربری</h2>
+
+      </div>
+      <div className="dashboard-left">
+        <h3 className="dashboard-right__title">اطلاعیه ها</h3>
+      </div>
     </div>
   );
 }
