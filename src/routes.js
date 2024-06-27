7f5a53c3753c0537c0a58a203e607b9436953348
Nima Ranjbar <nimaranjbarnoshari@gmail.com>
2024-06-27 16:08:24 +0330

    feat: start develope orderDetails page

--- src/routes.js
+++ src/routes.js
@@ -19,6 +19,7 @@ import Tickets from "./Pages/Panel/Tickets/Tickets"
 import User from "./Pages/Panel/User/User"
 import Wallet from "./Pages/Panel/Wallet/Wallet"
 import Notifications from "./Pages/Panel/Notifications/Notifications"
+import OrderDetails from "./Pages/OrderDetails/OrderDetails";
 
 import { Navigate } from "react-router-dom";
 
@@ -42,6 +43,7 @@ const routes = [
         {path: "" ,element: <Navigate to="dashboard"/>},
         {path:"dashboard" , element: <Dashboard />},
         {path:"orders" , element: <Orders />},
+        {path:"orders/:orderID" , element: <OrderDetails />},
         {path:"tickets" , element: <Tickets />},
         {path:"user" , element: <User />},
         {path:"wallet" , element: <Wallet />},
