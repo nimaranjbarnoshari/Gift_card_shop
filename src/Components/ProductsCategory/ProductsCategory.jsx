af5242f4ea1acdedd304487834e00f2af42238cf
Nima Ranjbar <nimaranjbarnoshari@gmail.com>
2024-07-13 23:05:45 +0330

    feat: develope category section with json

--- src/Components/ProductsCategory/ProductsCategory.jsx
+++ src/Components/ProductsCategory/ProductsCategory.jsx
@@ -1,50 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./ProductsCategory.css";
 import SectionHeader from "../SectionHeader/SectionHeader";
 import AccountBox from "../AccountBox/AccountBox";
 
 export default function ProductsCategory() {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/categories")
+      .then((res) => res.json())
+      .then((allCategories) => {
+        setCategories(allCategories);   
+      });
+  }, []);
   return (
     <section className="products-category">
       <div className="container">
         <SectionHeader title="دسته بندی محصولات" />
 
         <div className="products-category__box-wrapper">
-          <AccountBox
-            src="/images/home/steam.jpg"
-            title="انواع گیفت کارت ها"
-            desc="انواع گیف کارت های استیم ، گوگل پلی ..."
-          />
-
-          <AccountBox
-            src="/images/home/popgi.jpg"
-            title="انواع بازی های دیجیتال"
-            desc="بازی های دیجیتال پلی استیشن ، ایکس باکس و ..."
-          />
-
-          <AccountBox
-            src="/images/home/coin.jpg"
-            title="انواع پول و آیتم بازی"
-            desc="انواع کوین های رایج بازی ها (cp) "
-          />
-
-          <AccountBox
-            src="/images/home/socials.jpg"
-            title="انواع خدمات و اکانت دیجیتال"
-            desc="اکانت اسپاتیفای ، پریمیوم سازی تلگرام و ..."
-          />
-
-          <AccountBox
-            src="/images/home/ping.jpg"
-            title="انواع نرم افزار ها"
-            desc="نرم افزار های کاهش پینگ و ..."
-          />
-
-          <AccountBox
-            src="/images/home/accessory.jpg"
-            title="انواع لوازم دیجیتال"
-            desc="کنسول بازی ، موس ، کیبورد و ..."
-          />
+          {categories.map((category) => (
+            <AccountBox
+              key={category.id}
+              src={category.src}
+              title={category.title}
+              desc={category.desc}
+            />
+          ))}
         </div>
       </div>
     </section>
