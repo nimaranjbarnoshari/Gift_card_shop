77b346382515524e4ec5d30d2d4e1def909542ea
Nima Ranjbar <nimaranjbarnoshari@gmail.com>
2024-07-15 23:53:29 +0330

    feat: prepare context

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,40 @@
 import "./App.css";
 import { useRoutes } from "react-router-dom";
-import routes from './routes'
+import routes from "./routes";
+import AuthContext from "./Context/AuthContext";
+import { useEffect, useState } from "react";
 
 function App() {
-  const router = useRoutes(routes)
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [token, setToken] = useState(null);
+  const [userInfos, setUserInfos] = useState({});
+
+  const login = ({ userData, userToken }) => {
+    setIsLoggedIn(true);
+    setUserInfos(userData);
+    localStorage.setItem("user", JSON.stringify({ userToken }));
+  };
+
+  const logout = () => {
+    setIsLoggedIn(false);
+    setToken(null);
+    localStorage.removeItem("user");
+  };
+
+  useEffect(() => {
+    const localStorageinfo = JSON.parse(localStorage.getItem("user"));
+
+    console.log(localStorageinfo);
+    if (localStorageinfo) {
+    }
+  }, []);
+  const router = useRoutes(routes);
   return (
-    <div className="App">
-      {router}
-    </div>
+    <AuthContext.Provider
+      value={{ isLoggedIn, token, userInfos, login, logout }}
+    >
+      <div className="App">{router}</div>
+    </AuthContext.Provider>
   );
 }
 
