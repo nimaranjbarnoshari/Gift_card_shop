924ef735e2a4bc704e1538a35fe1635ce905c7c0
Nima Ranjbar <nimaranjbarnoshari@gmail.com>
2024-07-27 22:17:09 +0330

    feat: develope validation of complete register page with formik and yup

--- src/Pages/FinishRegister/FinishRegister.jsx
+++ src/Pages/FinishRegister/FinishRegister.jsx
@@ -1,62 +1,70 @@
-import React, { useState, useContext } from "react";
+import React, { useContext } from "react";
 import Input from "../../Components/Form/Input";
 import Button from "../../Components/Form/‌Button";
 import { Link, useParams, useNavigate } from "react-router-dom";
 import Swal from "sweetalert2";
 import AuthContext from "../../Context/AuthContext";
+import { useFormik } from "formik";
+import registerSchema from "../../registerVlidation";
 import "./FinishRegister.css";
+
 export default function FinishRegister() {
   const { mobile } = useParams();
   const contextData = useContext(AuthContext);
   const navigate = useNavigate();
-  const [fullName, setFullName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [repeatPassword, setRepeatPassword] = useState("");
 
-  const completeSignup = async () => {
-    const token =
-      Math.floor(Math.random() * 1000000) +
-      email.slice(0, 6) +
-      Math.floor(Math.random() * 1000000) +
-      email +
-      Math.floor(Math.random() * 1000000);
+  const formik = useFormik({
+    initialValues: {
+      fullName: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
+    onSubmit: async ({ fullName, email, password, confirmPassword }) => {
+      const token =
+        Math.floor(Math.random() * 1000000) +
+        email.slice(0, 6) +
+        Math.floor(Math.random() * 1000000) +
+        email +
+        Math.floor(Math.random() * 1000000);
 
-    const user = {
-      fullName,
-      mobile,
-      email,
-      password,
-      token,
-    };
-
-    const res = await fetch(`http://localhost:8000/users`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(user),
-    });
-
-    if (res.ok) {
-      const response = await res.json();
-
-      Swal.fire({
-        title: "ثبت نام شما با موفقیت انجام شد",
-        icon: "success",
-      }).then(() => {
-        contextData.login(response, response.token);
-        navigate("/");
+      const user = {
+        fullName,
+        mobile,
+        email,
+        password,
+        token,
+      };
+      console.log(user);
+      const res = await fetch(`http://localhost:8000/users`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(user),
       });
-    } else {
-      Swal.fire({
-        title: "متاسفانه خطایی رخ داد",
-        color: "#fd295c",
-        icon: "error",
-        iconColor: "#fd295c",
-      });
-    }
-  };
+
+      if (res.ok) {
+        const response = await res.json();
+
+        Swal.fire({
+          title: "ثبت نام شما با موفقیت انجام شد",
+          icon: "success",
+        }).then(() => {
+          contextData.login(response, response.token);
+          navigate("/");
+        });
+      } else {
+        Swal.fire({
+          title: "متاسفانه خطایی رخ داد",
+          color: "#fd295c",
+          icon: "error",
+          iconColor: "#fd295c",
+        });
+      }
+    },
+    validationSchema: registerSchema,
+  });
 
   return (
     <div className="finishRegister">
@@ -80,49 +88,76 @@ export default function FinishRegister() {
                 لطفا برای ثبت نام اطلاعات زیر را کامل کنید
               </p>
             </div>
-            <form className="finishRegister-form">
+            <form
+              className="finishRegister-form"
+              onSubmit={formik.handleSubmit}
+            >
               <Input
                 id="fullName"
                 type="text"
                 label="نام و نام خانوادگی"
                 placeholder="نام و نام خانوادگی خود را وارد نمایید"
-                className="finishRegister-form__input-phone"
-                value={fullName}
-                onChange={(event) => setFullName(event.target.value)}
+                custom="finishRegister-form__input"
+                onChange={formik.handleChange}
+                value={formik.values.fullName}
+                onBlur={formik.handleBlur}
               />
+              {formik.errors.fullName && formik.touched.fullName && (
+                <span className="finishRegister-form__input-err">
+                  {formik.errors.fullName}
+                </span>
+              )}
               <Input
                 id="email"
                 type="email"
                 label="ایمیل"
                 placeholder="ایمیل خود را وارد نمایید"
-                className="finishRegister-form__input-phone"
-                value={email}
-                onChange={(event) => setEmail(event.target.value)}
+                custom="finishRegister-form__input"
+                onChange={formik.handleChange}
+                value={formik.values.email}
+                onBlur={formik.handleBlur}
               />
+              {formik.errors.email && formik.touched.email && (
+                <span className="finishRegister-form__input-err">
+                  {formik.errors.email}
+                </span>
+              )}
               <Input
                 id="password"
                 type="password"
                 label="رمز عبور"
                 placeholder="رمز عبور خود را وارد نمایید"
-                className="finishRegister-form__input-phone"
-                value={password}
-                onChange={(event) => setPassword(event.target.value)}
+                custom="finishRegister-form__input"
+                onChange={formik.handleChange}
+                value={formik.values.password}
+                onBlur={formik.handleBlur}
               />
+              {formik.errors.password && formik.touched.password && (
+                <span className="finishRegister-form__input-err">
+                  {formik.errors.password}
+                </span>
+              )}
               <Input
-                id="repeatPassword"
+                id="confirmPassword"
                 type="password"
                 label="تکرار رمز عبور"
                 placeholder="رمز عبور خود را تکرار نمایید"
-                className="finishRegister-form__input-phone"
-                value={repeatPassword}
-                onChange={(event) => setRepeatPassword(event.target.value)}
+                custom="finishRegister-form__input"
+                onChange={formik.handleChange}
+                value={formik.values.confirmPassword}
+                onBlur={formik.handleBlur}
               />
-
+              {formik.errors.confirmPassword &&
+                formik.touched.confirmPassword && (
+                  <span className="finishRegister-form__input-err">
+                    {formik.errors.confirmPassword}
+                  </span>
+                )}
               <Button
                 children="تکمیل ثبت نام"
-                type="button"
-                onClick={completeSignup}
+                type="submit"
                 className="finishRegister-form__link"
+                disabled={!formik.isValid}
               />
             </form>
 
---
+++ src/registerVlidation.js
@@ -0,0 +1,25 @@
+import * as Yup from "yup"
+
+const registerSchema = Yup.object().shape({
+
+    fullName: Yup.string()
+        .min(6, "نام کامل نباید کمتر از ۶ کاراکتر باشد")
+        .max(25, "نام کامل نباید بیشتر از ۲۵ کاراکتر باشد")
+        .required("وارد کردن شماره الزامی می باشد"),
+
+    email: Yup.string()
+        .min(10, "ایمیل نباید کمتر از ۸ کاراکتر باشد")
+        .max(30, "ایمیل نباید بیشتر از ۳۰ کاراکتر باشد")
+        .email("ایمیل معتبر نمی باشد")
+        .required("وارد کردن شماره الزامی می باشد"),
+
+    password: Yup.string()
+        .min(8, "رمز عبور نباید کمتر از ۸ کاراکتر باشد")
+        .max(20, "رمز  عبور نباید بیشتر از ۲۰ کاراکتر باشد")
+        .required("وارد کردن رمز عبور الزامی می باشد"),
+
+    confirmPassword: Yup.string()
+        .oneOf([Yup.ref('password')], "تکرار رمز عبور منطبق نیست")
+})
+
+export default registerSchema
